4a49e33deb88e45879f4588ccc2c5e28da4b800c
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-28 16:25:34 -0200

    Procfile for Heroku

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+/*$router->get('/key', function() {
+    return str_random(32);
+});*/
+
 $router->group(['prefix' => '/api/v1'], function($app){
 	$app->group(['prefix' => 'auth'], function($app){
 		$app->post('login/','AuthController@login');
